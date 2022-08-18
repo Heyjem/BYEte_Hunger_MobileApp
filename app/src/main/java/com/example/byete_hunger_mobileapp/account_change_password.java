a0bbc1ac048e34d5c8d51b0ef5ae0d37c4cb4461
JnxdTn <janxedmtan@gmail.com>
2022-08-18 16:24:52 +0800

    added account change password

---
+++ app/src/main/java/com/example/byete_hunger_mobileapp/account_change_password.java
@@ -0,0 +1,14 @@
+package com.example.byete_hunger_mobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class account_change_password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_change_password);
+    }
+}
