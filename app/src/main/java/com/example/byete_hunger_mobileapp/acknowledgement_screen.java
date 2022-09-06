0db5395d4d88143a86b35b6ea748ce110e9a92f6
FIRST_NAME LAST_NAME <lacruz0525@gmail.com>
2022-09-06 17:29:00 +0800

    Komeet

---
+++ app/src/main/java/com/example/byete_hunger_mobileapp/acknowledgement_screen.java
@@ -0,0 +1,14 @@
+package com.example.byete_hunger_mobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class acknowledgement_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acknowledgement_screen);
+    }
+}
---
+++ app/src/main/java/com/example/byete_hunger_mobileapp/welcome_screen.java
@@ -0,0 +1,14 @@
+package com.example.byete_hunger_mobileapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class welcome_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+}
