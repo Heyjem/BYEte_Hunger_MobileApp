80218d60e39a60c8573e4509378549375ea716ec
Heyjem <94209594+Heyjem@users.noreply.github.com>
2022-09-15 00:58:54 +0800

    commit chat screen

--- app/src/main/java/com/example/byete_hunger_mobileapp/Chat_Screen.java
+++ app/src/main/java/com/example/byete_hunger_mobileapp/Chat_Screen.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Chat_Screen2 extends AppCompatActivity {
+public class Chat_Screen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat_screen2);
+        setContentView(R.layout.activity_chat_screen);
     }
 }
